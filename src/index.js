b30245e06525201053b1a51c4f9c53ba5f548a0d
wenglin1 <wenglin@buffalo.edu>
2020-02-24 22:45:42 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskDescription = document.getElementById('new-task-description') 
+  const findList = document.querySelector("#tasks")
+  const newTaskForm = document.getElementById('create-task-form')
+
+  // event listeners 
+  newTaskForm.addEventListener("submit", event => {
+    event.preventDefault()
+
+    const newElement = document.createElement("li")
+    newElement.textContent = taskDescription.value
+    findList.append(newElement)
+
+    const deleteButton = document.createElement("button")
+    deleteButton.textContent = "X"
+    deleteButton.className = "Delete"
+    newElement.append(deleteButton)
+
+    deleteButton.addEventListener("click", event => {
+      newElement.remove()
+    })
+  })
+
+  
+
 });
